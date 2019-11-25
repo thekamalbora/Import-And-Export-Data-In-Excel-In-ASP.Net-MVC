077c877781da199a60f59fcccb3dac7fc930462f
Kamal Singh Bora <thekamalbora@yahoo.com>
2019-11-25 09:48:11 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Import_And_Export_Data_In_Excel_In_ASP.Net_MVC.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Import_And_Export_Data_In_Excel_In_ASP.Net_MVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
